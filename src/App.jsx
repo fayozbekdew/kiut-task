fa53cf0f3291bbf042c49798e8177c0f1841f11b
Fayozbek Borotboyev <fayozbekdev@gmail.com>
2025-04-03 11:35:01 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { lazy } from "react";
 import MainLayout from "./layout/MainLayout";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import Car from "./pages/Car";
 import CarAdd from "./pages/CarAdd";
 import CarEdit from "./pages/CarEdit";
@@ -8,25 +12,29 @@ import CarEdit from "./pages/CarEdit";
 function App() {
   const router = createBrowserRouter([
     {
+      path: "/",
+      element: <Navigate to="/car" replace />, // "/" sahifasiga kirsalar "/car"ga o'tadi
+    },
+    {
       path: "/car",
       element: <MainLayout />,
       children: [
         {
           index: true,
-          path: "/car",
-          element: <Car />,
+          element: <Car />, // "/car" asosiy sahifa bo'ladi
         },
         {
-          path: "/car/add",
+          path: "add",
           element: <CarAdd />,
         },
         {
-          path: "/car/edit/:id",
+          path: "edit/:id",
           element: <CarEdit />,
         },
       ],
     },
   ]);
+
   return <RouterProvider router={router} />;
 }
 
